149600a84156f0f79cc7ad762639eb610a96fe6f
Terry chan <napoler2008@gmail.com>
2020-03-05 10:17:27 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- train.py
+++ train.py
@@ -293,9 +293,9 @@ def main():
                     # optimizer the net
                     optimizer.step()
                     scheduler.step()        # update parameters of net
-                    # optimizer.zero_grad()        # update parameters of net
+                    optimizer.zero_grad()        # update parameters of net
                     # scheduler.zero_grad()        # update parameters of net
-                    model.zero_grad()   # reset gradient
+                    # model.zero_grad()   # reset gradient
                     end = datetime.now()
                     print("epoch:",epoch + 1," piece_num:",piece_num,'/',num_pieces," step:",gradient_accumulation_run+1,'/',total_steps," loss:",loss.item(),'Time',end-now," s")
                     #  forward pass
