9e29fba9c2ad8bb448c2ad9a9819fe255c098f39
Terry chan <napoler2008@gmail.com>
2020-03-05 08:50:53 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- train.py
+++ train.py
@@ -272,15 +272,16 @@ def main():
                 # print(batch_labels)
 
                 # print(len(batch_inputs))
-                print(batch_inputs)
+                # print(batch_inputs)
                 # print(len(batch_inputs))
                 try:
-                    print("成功")
+                    # 
                     loss = model(batch_inputs, return_loss = True)
+                    print("成功")
                     # pred = model(batch_inputs)
                     # loss = loss_fn(pred.view(-1, full_tokenizer.vocab_size), batch_inputs.view(-1))
                     # print("计算loss",mlm_loss.item(),'返回loss',loss.item())
-                    print('返回loss',loss.item())
+                    # print('返回loss',loss.item())
                 except:
                     print("错误！")
                     continue
